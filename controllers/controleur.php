08bebfe648a507322285abde4601837084f67760
Aldric <goug.lab@gmail.com>
2016-09-27 19:36:46 +0200

    Correction orthographique.

--- controllers/controleur.php
+++ controllers/controleur.php
@@ -4,7 +4,7 @@
  * @author Aldric.L
  * @copyright Copyright 2016-2017 Aldric L.
  */
-//Cette classe sert de base à tous les controlleurs
+//Cette classe sert de base à tous les controleurs
   class Controleur{
     private $Serveur_Config = array();
     public $css;
